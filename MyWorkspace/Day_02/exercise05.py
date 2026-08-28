fb92ee8c5ce762217a6174974661d7b3324387f5
TejasRanjith <tejasranjith035611@gmail.com>
2026-08-28 11:48:22 +0530

    Fix input prompt formatting and enhance sentence capitalization logic exercise 05

--- MyWorkspace/Day_02/exercise05.py
+++ MyWorkspace/Day_02/exercise05.py
@@ -1,5 +1,12 @@
 def main():
-    sentence = input("Enter your sentence")
+    sentence = input("Enter your sentence : ")
+    output = ""
+    new = sentence.split()
+    for word in new:
+        output+= word[0].upper()
+        output+= word[1:].lower()
+        output+= " "
+    print(output)
 
 if __name__ == '__main__':
     main()
