fd165b90c2c8b7b85fb6cf638bac9f0d7ca88215
Tejas Ranjith <66506433+TejasRanjith@users.noreply.github.com>
2026-08-30 21:07:14 +0530

    Add solution for Day 03 exercise 09 (Josephus problem)

--- MyWorkspace/Day_03/exercise09.py
+++ MyWorkspace/Day_03/exercise09.py
@@ -1,9 +1,13 @@
 n = int(input("Enter the number of soldiers: "))
-soldiers = [n+1 for n in range(n)]
 k = int(input("Enter the Elimination Interval: "))
-i=0
-while n > 1 and i < 5:
-    print(soldiers.pop(i+k-1))
-    print(soldiers)
-    n-=1
-    i+=1
+output,remove = [],k-1
+soldiers = [n+1 for n in range(n)]
+
+print("Soldier circle initialized:",soldiers)
+
+for i in range(len(soldiers)-1):
+    print("Eliminated soldier:",soldiers.pop(remove),"  Remaining:",soldiers)
+    remove+=k-1
+    remove = remove%(n-(i+1))
+
+print("The sole survivor is:",soldiers[0])
