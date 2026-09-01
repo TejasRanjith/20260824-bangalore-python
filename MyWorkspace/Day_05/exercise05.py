2cf4ffce1874b79972a0fb2c79951300f8e99bb9
TejasRanjith <tejasranjith035611@gmail.com>
2026-09-01 16:57:49 +0530

    Fix bank account creation function by completing the withdraw method and ensuring proper transaction handling

--- MyWorkspace/Day_05/exercise05.py
+++ MyWorkspace/Day_05/exercise05.py
@@ -1,5 +1,4 @@
 AUDIT_TRANSACTION_COUNT = 0
-
 def create_bank_account(owner_name, initial_balance):
     global AUDIT_TRANSACTION_COUNT
     balance = float(initial_balance)
@@ -10,3 +9,20 @@ def create_bank_account(owner_name, initial_balance):
         balance += amount
         history.append(f"Deposit of {amount}")
         AUDIT_TRANSACTION_COUNT+=1
+    def withdraw(amount):
+        global AUDIT_TRANSACTION_COUNT
+        if balance>=amount:
+            balance-=amount
+            history.append(f"Withdrawal of {amount}")
+        else:
+            raise ValueError("Insufficient Balance")
+    
+    return {
+        "deposit":deposit,
+        "withdraw":
+    }
+
+acc = create_bank_account('tejas',10)
+acc[]
+
+            
