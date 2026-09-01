c6c11d3ec9491317eeff6b6519df9557ea3b6e92
TejasRanjith <tejasranjith035611@gmail.com>
2026-09-01 12:02:02 +0530

    Refactor dataset processing function to improve clarity and structure

--- MyWorkspace/Day_05/exercise04.py
+++ MyWorkspace/Day_05/exercise04.py
@@ -1,10 +1,27 @@
 import re
 def process_dataset(dataset):
+    result,output = [],[]
     for item in dataset:
+        item_dict = dict()
         for data in item:
-            m = re.search(r"[A-Za-z]+",data)
-            print(m.groups())
             
+            if re.findall(r"^[A-Za-z]+$",data):
+                item_dict["product"] = re.findall(r"^[A-Za-z]+$",data)[0]
+                
+            elif "Price" in re.split(r"(Price)[:\s]",data):
+                item_dict["price"] = float(re.split(r"(Price)[:\s]",data)[-1])
+                
+            elif "Rating" in re.split(r"(Rating)[:\s.]",data):
+                item_dict["score"] = float(re.split(r"(Rating)[:\s.]",data)[-1])
+                
+        result.append(item_dict)
+        
+    for item in filter(lambda x:x["price"]<1000,result):
+        output.append(item)
+    
+    
+    return output
+
             
 
 data_input = [
@@ -13,4 +30,4 @@ data_input = [
     ("Mouse", "Price: 25", "Rating: 4.7"),
     ("Charger", "Price: 15", "Rating: 4.2")
 ]
-process_dataset(data_input)
+print(process_dataset(data_input))
