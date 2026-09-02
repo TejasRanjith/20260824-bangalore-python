4b82bfdf35c7901a3e92c129daf0dd7852da4036
Tejas Ranjith <66506433+TejasRanjith@users.noreply.github.com>
2026-09-02 21:45:15 +0530

    Save Day 06 exercise01 progress in MyWorkspace

--- MyWorkspace/Day_06/exercise01.py
+++ MyWorkspace/Day_06/exercise01.py
@@ -2,4 +2,17 @@ class SmartThermostat():
     MIN_TEMP = 10.0
     MAX_TEMP = 35.0
     def __init__(self,appliance_name,initial_temp):
-        __appliance_name = appliance_name
+        self.__appliance_name = self.appliance_name
+        print(appliance_name)
+
+thermostat = SmartThermostat("Living Room AC", 24.0)
+print(thermostat.__appliance_name)  # Output: Living Room AC
+print(thermostat.target_temp)     # Output: 24.0
+
+thermostat.target_temp = 28.0     # Updates successfully
+print(thermostat.target_temp)     # Output: 28.0
+
+try:
+    thermostat.target_temp = 5.0  # Out of range!
+except ValueError as e:
+    print(e)  # Output: Temperature must be between 10.0 and 35.0 degrees.
